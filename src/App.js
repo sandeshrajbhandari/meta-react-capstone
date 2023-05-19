3e05b161624aee19170c29bb6ecb14eed5d00dab
sandeshrajbhandari <sandeshrb87@gmail.com>
2023-05-19 15:54:44 +0545

    basic react components setup and meta tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      Homepage
+    <div>
+      <div>test</div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+
+const Nav = () => {
+  return (
+    <>
+        <ul>
+            <li>Home</li>
+            <li>Menu</li>
+        </ul>
+    </>
+  )
+}
+
+export default Nav
